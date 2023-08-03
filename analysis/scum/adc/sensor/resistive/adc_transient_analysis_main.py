1c7b35f47417a8feecb4d0e5d4b330d49c0a3bef
Titan Yuan <tryuan99@gmail.com>
2023-08-03 14:28:36 -0700

    Plot the differences for approximations

--- analysis/scum/adc/sensor/resistive/adc_transient_analysis_main.py
+++ analysis/scum/adc/sensor/resistive/adc_transient_analysis_main.py
@@ -41,8 +41,9 @@ def _generate_transient_adc_data(tau: float,
     return np.round(exponential + noise)
 
 
-def plot_sample_distribution() -> None:
-    """Plots the distribution of the log ADC samples as a function of tau."""
+def plot_log_noise_distribution() -> None:
+    """Plots the distribution of the noise of the log ADC samples."""
+    # Plot the distribution of the noise.
     fig, ax = plt.subplots(figsize=(12, 8))
     x = np.linspace(-0.25, 0.25, 10000)
     for tau in TAUS:
@@ -61,20 +62,43 @@ def plot_sample_distribution() -> None:
                  "--",
                  c=pdf_plot[0].get_color(),
                  label=f"tau={tau} (approximated)")
-    ax.set_title("Probability density of the log ADC samples")
-    ax.set_xlabel("Difference from actual value")
+    ax.set_title("Probability density of the noise of the log ADC samples")
+    ax.set_xlabel("Noise value [bits]")
     ax.set_ylabel("Probability density")
     plt.legend()
     plt.show()
 
+    # Plot the difference between the actual distribution and the approximated
+    # distribution.
+    fig, ax = plt.subplots(figsize=(12, 8))
+    x = np.linspace(-0.25, 0.25, 10000)
+    for tau in TAUS:
+        pdf = 1 / (np.sqrt(2 * np.pi) * SIGMA) * np.exp(
+            -(EXPONENTIAL_SCALING_FACTOR**2 * np.exp(-2 * tau) *
+              (np.exp(x) - 1)**2) /
+            (2 * SIGMA**2)) * EXPONENTIAL_SCALING_FACTOR * np.exp(x - tau)
 
-def plot_sample_stddev() -> None:
-    """Plots the standard deviation of the log ADC samples as a function of tau."""
+        # Approximate the pdf.
+        stddev = SIGMA * np.exp(tau) / EXPONENTIAL_SCALING_FACTOR
+        pdf_approximated = 1 / (np.sqrt(2 * np.pi) * stddev) * np.exp(
+            -1 / 2 * x**2 / stddev**2)
+        plt.plot(x, pdf_approximated - pdf, label=f"tau={tau}")
+    ax.set_title(
+        "Approximation error of the probability density of the noise of the log ADC samples"
+    )
+    ax.set_xlabel("Noise value [bits]")
+    ax.set_ylabel("Difference in the probability density")
+    plt.legend()
+    plt.show()
+
+
+def plot_log_noise_stddev() -> None:
+    """Plots the standard deviation of the noise of the log ADC samples."""
     fig, ax = plt.subplots(figsize=(12, 8))
     tau = np.linspace(np.min(TAUS), np.max(TAUS), 10000)
     stddev = SIGMA * np.exp(tau) / EXPONENTIAL_SCALING_FACTOR
     plt.plot(tau, stddev, label="Standard deviation")
-    ax.set_title("Standard deviation of the log ADC samples")
+    ax.set_title("Standard deviation of the noise of the log ADC samples")
     ax.set_xlabel("tau")
     ax.set_ylabel("Standard deviation [bits]")
     plt.legend()
@@ -95,6 +119,8 @@ def plot_slope_stddev(sampling_rate: float) -> None:
         weighted_linear_regression = adc_data.perform_weighted_linear_regression(
         )
         slope_stddevs.append(np.sqrt(weighted_linear_regression.slope_variance))
+
+    # Plot the standard deviation of the estimated slope.
     fig, ax = plt.subplots(figsize=(12, 8))
     plt.plot(taus, slope_stddevs, label="Empirical")
     plt.plot(taus,
@@ -104,6 +130,21 @@ def plot_slope_stddev(sampling_rate: float) -> None:
     ax.set_title("Standard deviation of the slope")
     ax.set_xlabel("Slope [1/s]")
     ax.set_ylabel("Standard deviation [1/s]")
+    plt.legend()
+    plt.show()
+
+    # Plot the difference between the actual standard deviation and the
+    # approximated standard deviation.
+    fig, ax = plt.subplots(figsize=(12, 8))
+    plt.plot(
+        taus,
+        np.sqrt(8 * SIGMA**2 /
+                (EXPONENTIAL_SCALING_FACTOR**2 * sampling_rate * taus**3)) -
+        slope_stddevs,
+        label="Approximated - actual")
+    ax.set_title("Approximation error of the standard deviation of the slope")
+    ax.set_xlabel("Slope [1/s]")
+    ax.set_ylabel("Difference in the standard deviation [1/s]")
     plt.legend()
     plt.show()
 
@@ -124,6 +165,8 @@ def plot_time_constant_stddev(sampling_rate: float) -> None:
         tau_stddev = np.sqrt(weighted_linear_regression.slope_variance /
                              np.abs(weighted_linear_regression.slope)**4)
         tau_stddevs.append(tau_stddev)
+
+    # Plot the standard deviation of the estimated time constant.
     fig, ax = plt.subplots(figsize=(12, 8))
     plt.plot(taus, tau_stddevs, label="Empirical")
     plt.plot(taus,
@@ -136,11 +179,26 @@ def plot_time_constant_stddev(sampling_rate: float) -> None:
     plt.legend()
     plt.show()
 
+    # Plot the difference between the actual time constant and the approximated
+    # time constant.
+    fig, ax = plt.subplots(figsize=(12, 8))
+    plt.plot(taus,
+             np.sqrt(8 * SIGMA**2 * taus /
+                     (EXPONENTIAL_SCALING_FACTOR**2 * sampling_rate)) -
+             tau_stddevs,
+             label="Approximated - actual")
+    ax.set_title(
+        "Approximation error of the standard deviation of the time constant")
+    ax.set_xlabel("Time constant [s]")
+    ax.set_ylabel("Difference in the standard deviation [s]")
+    plt.legend()
+    plt.show()
+
 
 def main(argv):
     assert len(argv) == 1
-    plot_sample_distribution()
-    plot_sample_stddev()
+    plot_log_noise_distribution()
+    plot_log_noise_stddev()
     plot_slope_stddev(FLAGS.sampling_rate)
     plot_time_constant_stddev(FLAGS.sampling_rate)
 
