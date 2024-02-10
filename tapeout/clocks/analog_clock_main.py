9147d4f1ef673505f93d62c8736833681c60aa1f
Titan Yuan <tryuan99@gmail.com>
2024-02-10 14:37:09 -0800

    Plot the analog clock frequency from schematic simulation

--- tapeout/clocks/analog_clock_main.py
+++ tapeout/clocks/analog_clock_main.py
@@ -9,20 +9,27 @@ FLAGS = flags.FLAGS
 ANALOG_CLOCK_NUM_TUNING_BITS = 16
 
 
-def plot_analog_clock_tuning(measured_data: str) -> None:
+def plot_analog_clock_tuning(measured_data: str, simulated_data: str) -> None:
     """Plots the analog clock frequency as a function of its tuning code.
 
     Args:
         measured_data: Measured data filename.
+        simulated_data: Simulated data filename.
     """
     df_measured = pd.read_csv(measured_data, comment="#")
-    tuning_code_column, frequency_column = df_measured.columns
+    df_simulated = pd.read_csv(simulated_data, comment="#")
 
     plt.style.use(["science", "grid"])
     fig, ax = plt.subplots(figsize=(12, 8))
 
     # Plot the measured data.
-    df_measured.plot.line(ax=ax, x=tuning_code_column, linewidth=2, marker="^")
+    df_measured.plot.line(ax=ax,
+                          x=df_measured.columns[0],
+                          linewidth=2,
+                          marker="^")
+
+    # Plot the simulated data.
+    df_simulated.plot.line(ax=ax, x=df_simulated.columns[0], linestyle="--")
 
     plt.xlabel(f"Analog tuning code ({ANALOG_CLOCK_NUM_TUNING_BITS} bits)")
     plt.ylabel("Frequency [MHz]")
@@ -33,12 +40,17 @@ def plot_analog_clock_tuning(measured_data: str) -> None:
 def main(argv):
     assert len(argv) == 1
 
-    plot_analog_clock_tuning(FLAGS.analog_clock_measured_data)
+    plot_analog_clock_tuning(FLAGS.analog_clock_measured_data,
+                             FLAGS.analog_clock_simulated_data)
 
 
 if __name__ == "__main__":
     flags.DEFINE_string("analog_clock_measured_data",
                         "tapeout/clocks/data/analog_clock_measured.csv",
                         "Analog clock measured data.")
+    flags.DEFINE_string(
+        "analog_clock_simulated_data",
+        "tapeout/clocks/data/analog_clock_schematic_simulated.csv",
+        "Analog clock simulated data.")
 
     app.run(main)
